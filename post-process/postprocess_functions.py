7935f3075580024b351c6235e8f81626f39b4fc2
Clemente Gotelli <96237432+cgotelli@users.noreply.github.com>
2022-06-13 09:30:52 +0200

    divided postprocess functions from process

--- post-process/postprocess_functions.py
+++ post-process/postprocess_functions.py
@@ -102,22 +102,4 @@ def make_binary(w, h, x_links, y_links, printBinary, postProcessPath, name):
     
     return binary
 
-# PARAMETERS ------------------------------------------------------------------
-matfilePath = "/home/cgotelli/Documents/ttga_DEM/output/matfiles/links01_postprocess.mat"
-DEM_imagePath = "/home/cgotelli/Documents/ttga_DEM/toProcess/rescaled_dsm02.png"
-postProcessPath = "/home/cgotelli/Documents/ttga_DEM/output/"
-
-Delta = 1
-
-# BOOLEANS --------------------------------------------------------------------
-printBinary = True
-
-
-# PROCESS ---------------------------------------------------------------------
-
-
-links, x_links, y_links = load_matfile(matfilePath, Delta)
-background, w, h, c = load_background(DEM_imagePath)
-name = "test" # This should come from the for-loop for each file to postprocess
-binary = make_binary(w, h, x_links, y_links, printBinary, postProcessPath, name)
 
