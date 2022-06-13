3654b6222ef20d6fd78528fece20b81ccb70b28d
Clemente Gotelli <96237432+cgotelli@users.noreply.github.com>
2022-06-13 11:11:28 +0200

    Update postprocess_functions.py

--- post-process/postprocess_functions.py
+++ post-process/postprocess_functions.py
@@ -74,60 +74,6 @@ def load_background(DEMpath):
     (h, w,c) = np.shape(DEM)
     return DEM, w, h, c
 
-def plot_network (DEM, networks, Delta):
-    # Read the .mat file and the DEM :
-    data = loadmat(networks)
-    img = cv2.imread(DEM)
-    links = data['links']
-    
-    # Extract each column of the links matrix :
-    index_link = links[:,0]
-    delta_link = links[:,1]
-    x = links[:,2]
-    y = links[:,3]
-    
-    # Create two arrays that will be used to plot each link :
-    X = []
-    Y = []
-        
-    plt.figure(figsize=(30,10))
-    for i in range(len(index_link)):
-
-        # The same index value indicates the same link. We extract its coordinates :
-        if index_link[i]==index_link[i-1]:
-            X.append(x[i])
-            Y.append(y[i])
-
-<<<<<<< Updated upstream
-        else:
-        # If the index value change, we plot (X,Y) corresponding to the previous link :
-            if delta_link[i] > Delta or delta_link[i]=='inf':
-                lab = 'delta=' + str(delta_link[i-1])
-                ax = plt.subplot(111)
-                ax.plot(X, Y, label=lab)
-                
-            # Then we reset X amd Y
-            X = []
-            Y = []
-            X.append(x[i])
-            Y.append(y[i])
-=======
-# PARAMETERS ------------------------------------------------------------------
-matfilePath = "/home/lhe/Documents/PostProcess/savemat/link_seq_test_postprocess.mat"
-DEM_imagePath = "/home/lhe/Documents/ttga_DEM/DEMs/rescaled_dsm01.png"
-# PROCESS ---------------------------------------------------------------------
->>>>>>> Stashed changes
-
-    ax.imshow(img)
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0 + box.height * 0.1,
-                 box.width, box.height * 0.9])
-    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
-          fancybox=False, shadow=False, ncol=5)
-    
-    plt.title('networks ' +r'$\delta_{lim}=$'+' '+str(Delta))
-    plt.axis("off")
-    plt.show
     
 def makeFolder(postProcessPath, process):
     output_path = join(postProcessPath, process)
