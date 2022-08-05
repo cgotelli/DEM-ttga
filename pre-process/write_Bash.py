3ae0bac419e1515436114ec08130339c48a98bd0
Clemente Gotelli <96237432+cgotelli@users.noreply.github.com>
2022-08-05 15:03:42 +0200

    Update write_Bash.py
    include heading

--- pre-process/write_Bash.py
+++ pre-process/write_Bash.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # -------------------------------- IMPORT -------------------------------------
 from os import listdir, mkdir
 from os.path import isfile, join, exists
